70a63fd08e0bbfccb54d043cbe7f2c1110666644
Gaban98 <csrivera00@misena.edu.co>
2023-03-01 10:17:25 -0500

    commit bien realizado

--- polls/urls.py
+++ polls/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    #ex: /polls/
     path('', views.index, name='index'),
+    #el primer parametro es la url, el segundo es la vista que se va a renderizar
     path('<int:question_id>/', views.detail, name='detail'),
     #< .... > esa expresión es la forma en que django nos da de poder pasar variables o datos
     #si yo entro a /polls/5/ me va a mostrar la pregunta 5
-    path('<int:question_id>/results/', views.result, name='result'),
+    path('<int:question_id>/result/', views.result, name='result'),
     #ex: /polls/5/results/
     path('<int:question_id>/vote/', views.vote, name='vote'),
     #ex: /polls/5/vote/
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
 
 def index (request):
-    return HttpResponse("Pagina principal de la app de premios")
+    latest_question_list = Question.objects.all()
+    context = {'latest_question_list': latest_question_list}
+    #esto guarda en una variable la lista de preguntas
+    return render(request, "polls/index.html", context)
+# el primer parametro es el request, el segundo es el template que se va a renderizar
+#el tercer parametro es un diccionario que se le pasa al template
+#el diccionario es el contexto, es decir, las variables que se van a usar en el template
 
 
 def detail(request, question_id):
     #aca recibe de parametro el id de la pregunta
-    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+    return HttpResponse(f"Estas viendo el detalle pregunta numero {question_id}")
 
 def result(request, question_id):
     return HttpResponse(f"Estas viendo los resultados numero {question_id}")
